438bd6e0fe23c1e96e56af067d2c76153b84d56e
Nisha Bidave <nisha.bidave@cloudreach.com>
2022-08-03 15:40:39 +0530

    changes to bucket region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "talent-academy-pascal-lab-tfstates"
     key            = "talent-academy/vpc_group3/terraform.tfstates"
-    region         = "ap-south-1"
+    region         = "eu-west-1"
     dynamodb_table = "terraform-lock"
   }
 }
